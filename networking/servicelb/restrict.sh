24e7ccf99d23ed4ccc3271f4a01a37ddec726a32
Li Lin <guru.lin@gmail.com>
2023-05-23 22:03:28 +0800

    enable traefik and expose traefik and longhorn dashboards

--- networking/servicelb/restrict.sh
+++ networking/servicelb/restrict.sh
@@ -1,5 +1,5 @@
 # restrict servicelb only to 2 nodes in the cluster
 
+# may not work when traefik is ebnabked
 kubectl label nodes pie4 svccontroller.k3s.cattle.io/enablelb=true svccontroller.k3s.cattle.io/lbpool=pool1
 kubectl label nodes pie9 svccontroller.k3s.cattle.io/enablelb=true svccontroller.k3s.cattle.io/lbpool=pool2
-kubectl label nodes pie1 svccontroller.k3s.cattle.io/enablelb=true svccontroller.k3s.cattle.io/lbpool=pool2
